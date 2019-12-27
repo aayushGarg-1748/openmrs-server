7f6214396cd9b3e77ee9c935e54f8a13d7fdffc8
aayushGarg-1748 <argtech.1234@gmail.com>
2019-12-27 20:23:06 +0530

    GCI-gamer1748 for openmrs

--- openmrs-server/app.js
+++ openmrs-server/app.js
@@ -26,10 +26,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/info',(req, res) => {
+    res.json({
+        "ServerName" : `${ServerName}`,
+        "ServerVersion" : `${ServerVersion}`
+    });
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
