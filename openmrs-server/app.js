2851545f43bc49556e0b3ae2e2cbc27e86c0e60d
aayushGarg-1748 <argtech.1234@gmail.com>
2019-12-29 19:51:20 +0530

    GCI-gamer1748 by gamer1748 during gci for openmrs

--- openmrs-server/app.js
+++ openmrs-server/app.js
@@ -7,22 +7,44 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var productInfo = ["id"]["categoryId"];
+//var productInfo = ["id"]["categoryId"];
 
 //info
-var ServerData = require('./package.json');
-var ServerVersion = ServerData.version;
-var ServerName = ServerData.name;
+var serverData = require('./package.json');
+var serverVersion = serverData.version;
+var serverName = serverData.name;
 
 //products
 var productData = require('./src/data/products.json');
-var AllProductData = productData.products;
+var allProductData = productData.products;
 
 //categories
 var categoryData = require('./src/data/category.json');
-var AllCategoryData = categoryData.categories;
+var allCategoryData = categoryData.categories;
 
 const app = express();
+
+const products = [
+  {name: 'P001', place: 0},
+  {name: 'P002', place: 1},
+  {name: 'P003', place: 2},
+  {name: 'P004', place: 3},
+  {name: 'P005', place: 4},
+  {name: 'P006', place: 5},
+  {name: 'P007', place: 6},
+  {name: 'P008', place: 7},
+  {name: 'P009', place: 8},
+  {name: 'P010', place: 9},
+  {name: 'P011', place: 10},
+  {name: 'P012', place: 11},
+];
+
+const categories = [
+  {name: 'cty01', place: 0, product1: 'P001', product2:'P002', product3:'P003'},
+  {name: 'cty02', place: 1, product1: 'P004', product2:'P005', product3:'P006'},
+  {name: 'cty03', place: 2, product1: 'P007', product2:'P008', product3:'P009'},
+  {name: 'cty04', place: 3, product1: 'P010', product2:'P011', product3:'P012'}
+];
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -39,11 +61,11 @@ app.use('/users', usersRouter);
 
 app.get('/info',(req, res) => {
 
-    if((req.query.category) == "1"){
+    if((req.query.password) == "Admin123"){
 
         res.json({"happy hacking gamer-1748":[{
-                  "ServerName" : `${ServerName}`,
-                   "ServerVersion" : `${ServerVersion}`
+                  "ServerName" : `${serverName}`,
+                   "ServerVersion" : `${serverVersion}`
                    }]});
     }
     else{
@@ -55,110 +77,112 @@ app.get('/info',(req, res) => {
 
 });
 
-app.get('/product/:id', (req, res) => {
+app.get('/product/all', (req, res) => {
+    var prd1 = getProductData(0);
+    var prd2 = getProductData(1);
+    var prd3 = getProductData(2);
+    var prd4 = getProductData(3);
+    var prd5= getProductData(4);
+    var prd6 = getProductData(5);
+    var prd7 = getProductData(6);
+    var prd8 = getProductData(7);
+    var prd9 = getProductData(8);
+    var prd10 = getProductData(9);
+    var prd11 = getProductData(10);
+    var prd12 = getProductData(11);
+    var allprd = ({prd1, prd2 , prd3 , prd4 , prd5 , prd6 , prd7 , prd8 , prd9 , prd10 , prd11 , prd12 , allCategoryData});
+    res.send(allprd);
+    });
 
-    if((req.params.id) == "P001"){
-        res.json(AllProductData[0]);
-    }
-    else if((req.params.id) == "P002"){
-        res.json(AllProductData[1]);
-    }
-    else if((req.params.id) == "P003"){
-        res.json(AllProductData[2]);
-    }
-    else if((req.params.id)== "P004"){
-        res.json(AllProductData[3]);
-    }
-    else if((req.params.id) == "P005"){
-        res.json(AllProductData[4]);
-    }
-    else if((req.params.id) == "P006"){
-       res.json(AllProductData[5]);
-    }
-    else if((req.params.id) == "P007"){
-        res.json(AllProductData[6]);
-    }
-    else if((req.params.id) == "P008"){
-        res.json(AllProductData[7]);
-    }
-    else if((req.params.id) == "P009"){
-        res.json(AllProductData[8]);
-    }
-    else if((req.params.id) == "P010"){
-        res.json(AllProductData[9]);
-    }
-    else if((req.params.id) == "P011"){
-        res.json(AllProductData[10]);
-    }
-    else if((req.params.id) == "P012"){
-        res.json(AllProductData[11]);
-    }
-    else if ((req.params.id) == "all"){
-        res.json({AllProductData,AllCategoryData});
+app.get('/product/:id', (req, res) => {
+    var id = (req.params.id);
+    var productInfo = getProduct(id);
+    if (productInfo != null){
+        res.json(allProductData[productInfo]);
     }
     else{
-        res.send("no product of this id: error 404")
+        res.send("error404: not found");
     }
 });
 
+
 app.get('/category/:id', (req, res) => {
-    if ((req.params.id) == "cty01"){
-        var details = getCategory(0);
-        var product1 = getProductData(0);
-        var product2 = getProductData(1);
-        var product3 = getProductData(2);
-        res.json({category1: [{details, product1 ,product2 , product3}]});
+    var id = (req.params.id);
+    var categorInfo = getCategoryDetails(id);
+    if (categorInfo != null){
+        res.json(categorInfo);
     }
-    else if ((req.params.id) == "cty02"){
-             var details = getCategory(1);
-             var product1 = getProductData(3);
-             var product2 = getProductData(4);
-             var product3 = getProductData(5);
-             res.json({category2: [{details, product1 ,product2 , product3}]});
+    else{
+        res.send("error404: category not found");
     }
-    else if ((req.params.id) == "cty03"){
-             var details = getCategory(2);
-             var product1 = getProductData(6);
-             var product2 = getProductData(7);
-             var product3 = getProductData(8);
-             res.json({category3: [{details, product1 ,product2 , product3}]});
-    }
-    else if ((req.params.id) == "cty04"){
-             var details = getCategory(3);
-             var product1 = getProductData(9);
-             var product2 = getProductData(10);
-             var product3 = getProductData(11);
-             res.json({category4: [{details, product1 ,product2 , product3}]});
-    }
-    else {
-        res.send("No Category Of this Id");
-    }
+
 });
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-function getCategory(intege, categoryDta) {
+function getProductData(intege, ProductDta) {
+    var categoryName = null;
+
+    if(categoryName == null){
+        var data = (categories.find( ({ name }) => name === allProductData[intege]["categoryId"] ));
+        var tempPlace = data["place"];
+        categoryName = allCategoryData[tempPlace]["categoryName"];
+}
     return(
-    categoryDta = {
-    "id" : `${AllCategoryData[intege]["id"]}`,
-    "categoryName" : `${AllCategoryData[intege]["categoryName"]}`
+                ProductDta = {
+                "id" : `${allProductData[intege]["id"]}`,
+                "categoryId" : `${allProductData[intege]["categoryId"]}`,
+                "categoryName" : `${categoryName}`,
+                "name" : `${allProductData[intege]["name"]}`,
+                "unit" : `${allProductData[intege]["unit"]}`,
+                "pricePerUnit" : `${allProductData[intege]["pricePerUnit"]}`,
+                "stocks" : `${allProductData[intege]["stocks"]}`
+                }
+        )
+    categoryName = null;
+}
+
+function getProduct (productName, place){
+    var data = (products.find( ({ name }) => name === productName ));
+    var finalData = null;
+    if (data != null){
+        finalData = data["place"];
+    } else {
+        finalData = null;
     }
+    return(
+        place = finalData
     )
 }
 
-function getProductData(intege, ProductDta) {
-    return(
-    ProductDta = {
-    "id" : `${AllProductData[intege]["id"]}`,
-    "categoryId" : `${AllProductData[intege]["categoryId"]}`,
-    "name" : `${AllProductData[intege]["name"]}`,
-    "unit" : `${AllProductData[intege]["unit"]}`,
-    "pricePerUnit" : `${AllProductData[intege]["pricePerUnit"]}`,
-    "stocks" : `${AllProductData[intege]["stocks"]}`
+function getCategoryDetails (categoryName, place){
+    var data = (categories.find( ({ name }) => name === categoryName ));
+    var placeno = null;
+
+    if (data != null){
+        placeno = data["place"];
+        var prdct1 = data["product1"];
+        var prdct2 = data["product2"];
+        var prdct3 = data["product3"];
+
+        var product1place = getProduct(prdct1);
+        var product2place = getProduct(prdct2);
+        var product3place = getProduct(prdct3);
+
+        var details = allCategoryData[placeno];
+        var product1 = allProductData[product1place];
+        var product2 = allProductData[product2place];
+        var product3 = allProductData[product3place];
+        return(
+            place = ({categoryName, details, product1 ,product2 , product3})
+        )
+    } else {
+        return(null)
     }
-    )
+
+
 }
 
 // error handler
