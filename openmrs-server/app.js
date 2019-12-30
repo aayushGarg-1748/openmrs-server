b65b17a2504d492ce2ed32b17d90bb3d5d7b8df1
aayushGarg-1748 <argtech.1234@gmail.com>
2019-12-30 23:11:27 +0530

    GCI-gamer-1748 for openmrs

--- openmrs-server/app.js
+++ openmrs-server/app.js
@@ -24,27 +24,12 @@ var allCategoryData = categoryData.categories;
 
 const app = express();
 
-const products = [
-  {name: 'P001', place: 0},
-  {name: 'P002', place: 1},
-  {name: 'P003', place: 2},
-  {name: 'P004', place: 3},
-  {name: 'P005', place: 4},
-  {name: 'P006', place: 5},
-  {name: 'P007', place: 6},
-  {name: 'P008', place: 7},
-  {name: 'P009', place: 8},
-  {name: 'P010', place: 9},
-  {name: 'P011', place: 10},
-  {name: 'P012', place: 11},
-];
-
 const categories = [
-  {name: 'cty01', place: 0, product1: 'P001', product2:'P002', product3:'P003'},
-  {name: 'cty02', place: 1, product1: 'P004', product2:'P005', product3:'P006'},
-  {name: 'cty03', place: 2, product1: 'P007', product2:'P008', product3:'P009'},
-  {name: 'cty04', place: 3, product1: 'P010', product2:'P011', product3:'P012'}
-];
+    {name: 'cty01', place: 0},
+    {name: 'cty02', place: 1},
+    {name: 'cty03', place: 2},
+    {name: 'cty04', place: 3}
+    ];
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -78,37 +63,25 @@ app.get('/info',(req, res) => {
 });
 
 app.get('/product/all', (req, res) => {
-    var prd1 = getProductData(0);
-    var prd2 = getProductData(1);
-    var prd3 = getProductData(2);
-    var prd4 = getProductData(3);
-    var prd5= getProductData(4);
-    var prd6 = getProductData(5);
-    var prd7 = getProductData(6);
-    var prd8 = getProductData(7);
-    var prd9 = getProductData(8);
-    var prd10 = getProductData(9);
-    var prd11 = getProductData(10);
-    var prd12 = getProductData(11);
-    var allprd = ({prd1, prd2 , prd3 , prd4 , prd5 , prd6 , prd7 , prd8 , prd9 , prd10 , prd11 , prd12 , allCategoryData});
-    res.send(allprd);
-    });
+    res.send(getAllProducts());
+});
 
 app.get('/product/:id', (req, res) => {
     var id = (req.params.id);
     var productInfo = getProduct(id);
+
     if (productInfo != null){
-        res.json(allProductData[productInfo]);
+        res.json(productInfo);
     }
     else{
         res.send("error404: not found");
     }
 });
 
-
 app.get('/category/:id', (req, res) => {
     var id = (req.params.id);
     var categorInfo = getCategoryDetails(id);
+
     if (categorInfo != null){
         res.json(categorInfo);
     }
@@ -122,33 +95,39 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-function getProductData(intege, ProductDta) {
+function getProductData(intege) {
     var categoryName = null;
+    var data = allProductData[intege];
 
     if(categoryName == null){
-        var data = (categories.find( ({ name }) => name === allProductData[intege]["categoryId"] ));
-        var tempPlace = data["place"];
+        var da = (categories.find( ({ name }) => name === allProductData[intege]["categoryId"] ));
+        var tempPlace = da["place"];
         categoryName = allCategoryData[tempPlace]["categoryName"];
-}
-    return(
-                ProductDta = {
-                "id" : `${allProductData[intege]["id"]}`,
-                "categoryId" : `${allProductData[intege]["categoryId"]}`,
-                "categoryName" : `${categoryName}`,
-                "name" : `${allProductData[intege]["name"]}`,
-                "unit" : `${allProductData[intege]["unit"]}`,
-                "pricePerUnit" : `${allProductData[intege]["pricePerUnit"]}`,
-                "stocks" : `${allProductData[intege]["stocks"]}`
-                }
-        )
+    }
+
+    data.categoryName = categoryName;
+    return(data);
     categoryName = null;
 }
 
-function getProduct (productName, place){
-    var data = (products.find( ({ name }) => name === productName ));
+function getProduct (productName){
+    var categoryName = null;
+
+    var data = (allProductData.find( ({ id }) => id === productName ));
+//var index = (allProductData.indexOf(data));
+//console.log(index);
     var finalData = null;
+
+    if(categoryName == null){
+            var da = (categories.find( ({ name }) => name === data["categoryId"] ));
+            var tempPlace = da["place"];
+            categoryName = allCategoryData[tempPlace]["categoryName"];}
+
     if (data != null){
-        finalData = data["place"];
+        //arr.push({'categoryName':`${categoryName}`});
+        data.categoryName = categoryName;
+
+        finalData = data;
     } else {
         finalData = null;
     }
@@ -157,32 +136,44 @@ function getProduct (productName, place){
     )
 }
 
-function getCategoryDetails (categoryName, place){
+function getCategoryDetails (categoryName){
     var data = (categories.find( ({ name }) => name === categoryName ));
     var placeno = null;
 
     if (data != null){
-        placeno = data["place"];
-        var prdct1 = data["product1"];
-        var prdct2 = data["product2"];
-        var prdct3 = data["product3"];
+        var arrayLength = allProductData.length;
+        var allDataOfProducts = [];
 
-        var product1place = getProduct(prdct1);
-        var product2place = getProduct(prdct2);
-        var product3place = getProduct(prdct3);
+        for (var i = 0; i < arrayLength; i++) {
+            allDataOfProducts.push(getProductData(i));
+        }
 
-        var details = allCategoryData[placeno];
-        var product1 = allProductData[product1place];
-        var product2 = allProductData[product2place];
-        var product3 = allProductData[product3place];
+        console.log(allDataOfProducts);
+        var finder = (allDataOfProducts.filter( ({ categoryId }) => categoryId === categoryName));
+        console.log(finder);
         return(
-            place = ({categoryName, details, product1 ,product2 , product3})
+            place = (finder)
         )
-    } else {
-        return(null)
+    }
+
+    else {
+        return(null);
     }
 
 
+}
+
+function getAllProducts(){
+    //const allProductPlaces = Object.keys(products);
+    //console.log(allProductPlaces);
+    //var iterator = allProductPlaces.values();
+    var arrayLength = allProductData.length;
+    var allDataOfProducts = [];
+    for (var i = 0; i < arrayLength; i++) {
+        allDataOfProducts.push(getProductData(i));
+        //Do something
+    }
+    return(allDataOfProducts);
 }
 
 // error handler
